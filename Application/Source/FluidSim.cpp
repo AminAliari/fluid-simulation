bf4e6f655e424aa932f673e1dcaee47223dbe37a
ama <mr.amin47@yahoo.com>
2022-10-15 13:38:13 -0400

    fix.

--- Application/Source/FluidSim.cpp
+++ Application/Source/FluidSim.cpp
@@ -7,6 +7,7 @@
 #define SPH_PARTITION_BUCKET_COUNT 1048576U
 
 // Interfaces
+#define USE_POSITIVE_PLANE_SIGN
 #include "../../TheForge/Utilities/Interfaces/ILog.h"
 #include "../../TheForge/Utilities/Interfaces/ITime.h"
 #include "../../TheForge/Game/Interfaces/IScripting.h"
@@ -23,7 +24,6 @@
 #include "../../TheForge/Resources/ResourceLoader/Interfaces/IResourceLoader.h"
 
 // Math
-#define USE_POSITIVE_PLANE_SIGN
 #include "../../TheForge/Utilities/Math/MathTypes.h"
 #include "../../TheForge/Utilities/Interfaces/IMemory.h"
 
